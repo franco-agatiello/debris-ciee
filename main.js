77d2ed65b1e9c2ea09b0f389f4d6ddb84b7bc908
frrannkko <121842945+franco-agatiello@users.noreply.github.com>
2025-06-06 22:48:35 -0300

    Update main.js

--- main.js
+++ main.js
@@ -177,14 +177,22 @@ function mostrarLeyendaCalor() {
 
 function initMapa() {
   mapa = L.map('map').setView([0, 0], 2);
-L.tileLayer(
-  'https://wms.ign.gob.ar/geoserver/ows?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=IGNBaseTodo&STYLE=default&TILEMATRIXSET=GoogleMapsCompatible&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image/png',
-  {
-    attribution: 'Mapas © Instituto Geográfico Nacional de la República Argentina',
-    maxZoom: 18,
-    tileSize: 256
-  }
-).addTo(mapa);
+
+  // IGN Argentina / CONAE
+  L.tileLayer(
+    'https://wms.ign.gob.ar/geoserver/ows?SERVICE=WMTS&REQUEST=GetTile&VERSION=1.0.0&LAYER=IGNBaseTodo&STYLE=default&TILEMATRIXSET=GoogleMapsCompatible&TILEMATRIX={z}&TILEROW={y}&TILECOL={x}&FORMAT=image/png',
+    {
+      attribution: 'Mapas © Instituto Geográfico Nacional de la República Argentina',
+      maxZoom: 18,
+      tileSize: 256
+    }
+  ).addTo(mapa);
+
+  // Etiqueta personalizada "Islas Malvinas"
+  L.marker([-51.7, -59], {interactive: false, opacity: 0})
+    .bindTooltip("Islas Malvinas", {permanent: true, direction: "center", className: "malvinas-label"})
+    .addTo(mapa);
+}
 
 function listeners() {
   ["pais", "material", "masa", "fecha-desde", "fecha-hasta", "inclinacion-min", "inclinacion-max"].forEach(id => {
